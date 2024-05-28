b32e3698f0a23be4548f165197ee3bac5902af2c
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-05-28 12:10:47 -0600

    rename application file, update properties and settings

--- src/main/java/com/example/reviewsservice/ReviewsApplication.java
+++ src/main/java/com/example/reviewsservice/ReviewsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewsServiceApplication {
+public class ReviewsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewsServiceApplication.class, args);
+		SpringApplication.run(ReviewsApplication.class, args);
 	}
 
 }
